0a699ea699f570deccae5deba58e2863186087c6
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-01 03:59:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,8 +5,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.new
   end
+
+  def create
+     @post = Post.new(post_params)
+     if @post.valid?
+       @post.save
+       redirect_to post_path(@post)
+     else
+       render :new
+     end
+   end
 
   def update
     @post = Post.find(params[:id])
