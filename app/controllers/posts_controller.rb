48611a5932dec278dec067ce21c63ecf10328861
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-03-01 04:20:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update(author_params)
+    if @post.update(post_params)
        redirect_to post_path(@post)
      else
        render :edit
